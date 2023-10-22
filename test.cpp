836f8e92f0842a0ef24804c97b33809730f4b46c
(NO2)3 C6H2CH3 <119799057+elakercy@users.noreply.github.com>
2023-10-22 12:29:09 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,m;
+	cin>>n;
+	if(n%2==0&&n!=2)
+	{
+		cout<<"YES"<<endl;
+	}
+	else
+	{
+		cout<<"NO"<<endl;
+	}
+	system("pause");
+	return 0;
+}
